3959cf2067bcb267aa65a1a3f87e7d92c75b5ae1
WithLsz <1067650156@qq.com>
2023-06-29 10:54:47 +0800

    test

---
+++ sampleapp1_backend/develop/module/Basic/sampleapp1-service-impl-basic/src/main/java/com/digiwin/sampleapp1/basic/service/impl/HelloworldService.java
@@ -0,0 +1,10 @@
+package com.digiwin.sampleapp1.basic.service.impl;
+
+import com.digiwin.sampleapp1.basic.service.IHelloworldService;
+
+public class HelloworldService implements IHelloworldService {
+    @Override
+    public String get(String name) throws Exception{
+        return name + ",Hello World!";
+    }
+}
